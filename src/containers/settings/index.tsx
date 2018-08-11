42fd7fed998e54d3dd9f28f863a0f6504bc4ef68
Jonathan <jonathancoder77@outlook.com>
2018-08-11 20:54:03 +1000

    finish account settings F.E mobile

--- src/containers/settings/index.tsx
+++ src/containers/settings/index.tsx
@@ -4,10 +4,15 @@ import styled from 'styled-components'
 import copy from '../../img/icons/copy.svg'
 import eyehide from '../../img/icons/eyehide.svg'
 import eyereveal from '../../img/icons/eyereveal.svg'
+import warning from '../../img/icons/warning.svg'
+import tick from '../../img/icons/tick.svg'
+import Slider from 'rc-slider';
+import ButtonStyle from '../../components/styles/Button'
 
+import 'rc-slider/assets/index.css';
 interface IAccountSettingsProps {
     accountSettings: {
-        feastsDistance: string,
+        feastsDistance: number,
         email: string,
         password: string,
         profileUrl: string,
@@ -23,7 +28,7 @@ class AccountSettings extends React.Component<IAccountSettingsProps, IAccountSet
 
     public state = {
         accountSettings: {
-            feastsDistance: '',
+            feastsDistance: 0,
             email: '',
             password: '',
             profileUrl: 'https://zestly.app/usHLr32D',
@@ -55,14 +60,41 @@ class AccountSettings extends React.Component<IAccountSettingsProps, IAccountSet
         document.execCommand('copy')
     }
 
+    public handleDistanceChange = (value: number) => {
+        this.setState({ ...this.state, accountSettings: { ...this.state.accountSettings, feastsDistance: value } })
+    }
+
+    public confirmChanges = () => {
+        console.log('succesfully saved')
+    }
+
+    public exit = () => {
+        console.log('leaving byeeeee')
+    }
+
+    public deleteAccount = () => {
+        console.log('deleting account byeeeee')
+    }
+
     public render() {
-        const { email, password, profileUrl } = this.state.accountSettings
+        const { email, password, profileUrl, feastsDistance } = this.state.accountSettings
 
         return (
             <div>
                 <LogoNav backPage={true} />
-                <h2>Account settings</h2>
-
+                <Heading>Account settings</Heading>
+                <InputGroup>
+                    <label htmlFor="feastsRange">
+                        Find feasts up to
+                    </label>
+                    <DistanceDetails>
+                        <p>Distance</p>
+                        <p>{feastsDistance} km</p>
+                    </DistanceDetails>
+                    <Distance>
+                        <Slider onChange={this.handleDistanceChange} value={feastsDistance} max={100} />
+                    </Distance>
+                </InputGroup>
 
                 <InputGroup>
                     <label htmlFor="accountEmail">
@@ -96,12 +128,30 @@ class AccountSettings extends React.Component<IAccountSettingsProps, IAccountSet
 
                 </InputGroup>
 
+                <EditButtons>
+                    <p onClick={this.exit}>Discard changes</p>
+                    <ConfirmButton onClick={this.confirmChanges}>
+                        <img src={tick} alt="confirm changes" />
+                        Save changes
+                    </ConfirmButton>
+                </EditButtons>
 
 
+                <DeleteAccount onClick={this.deleteAccount}>
+                    <p>Whoa there's no going back from this</p>
+                    <div>
+                        <img src={warning} alt="warning" />
+                        <p>Delete account</p>
+                    </div>
+                </DeleteAccount>
             </div>
         )
     }
 }
+
+const Heading = styled.h2`
+    margin: 0px 0px 24px 9px;
+`
 
 const InputGroup = styled.div`
     margin: 15px 0px;
@@ -117,7 +167,6 @@ const InputGroup = styled.div`
             margin-bottom: 11px;
         }
     }
-
     p {
         margin-left: 9px;
         color: ${props => props.theme.grey};
@@ -126,7 +175,7 @@ const InputGroup = styled.div`
     }
 `
 const InputBlock = styled.div`
-
+    background-color: #FAFAFA;
     height: 48px;
     border-top: 1px solid ${props => props.theme.grey};
     border-bottom: 1px solid ${props => props.theme.grey};
@@ -142,7 +191,7 @@ const InputBlock = styled.div`
         border: none;
         width: 95%;
         outline: none;
-    
+        background-color: #FAFAFA;
 }
     }
 
@@ -151,7 +200,81 @@ const InputBlock = styled.div`
         width: 20px;
         cursor: pointer;
     }
+`
+
+const Distance = InputBlock.extend`
+    border: none;
+    background-color: #fff;
+    .rc-slider-track {
+        background-color: ${props => props.theme.blueFaded};
+    }
+
+    .rc-slider-handle {
+        background-color: ${props => props.theme.blue};
+        border: none;
+    }
+    input {
+        background-color: #fff;
+    }
+`
+
+const DistanceDetails = styled.div`
+    display: flex;
+    justify-content: space-between;
+    p:last-child {
+        color: ${props => props.theme.purple};
+        margin-right: 9px;
+        font-weight: 500;
+    }
+`
+
+const EditButtons = styled.div`
+    padding: 2.5rem 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    p:first-child {
+        color: ${props => props.theme.red};
+        cursor: pointer;
+    }
+`
+
+const ConfirmButton = ButtonStyle.extend`
+    display: flex;
+    height: 4.8rem;	
+    width: 16.873rem;
+    background-color: ${props => props.theme.green};
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+        margin: 0px 7px 5px 0px;
+    }
+`
+
+const DeleteAccount = styled.div`
+    cursor: pointer;
+    text-align: center;
+    > p {
+        font-size: 1.4rem;
+        color: ${props => props.theme.grey};
+        padding: 10px;
+    }
+    div {
+        background-color: ${props => props.theme.red};
+        color: #fff;
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        img {
+            margin: 0px 7px 5px 0px;
+        }
+        text-transform: uppercase;
+    }
 
 `
+
 
 export default AccountSettings
