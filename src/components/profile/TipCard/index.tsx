28c3a02ba4f72752048a9eaebdaafdda789d7def
Jonathan <jonathancoder77@outlook.com>
2018-08-19 20:46:52 +1000

    start profile hub and create TipCard component

---
+++ src/components/profile/TipCard/index.tsx
@@ -0,0 +1,85 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import message from './message.svg'
+import people from './people.svg'
+import puzzle from './puzzle.svg'
+
+interface ITipCardProps {
+    color: string,
+    heading: string,
+    copy: string
+}
+const TipCard: React.SFC<ITipCardProps> = (props: ITipCardProps) => {
+    const determineStyle = (color: string) => {
+        switch (color) {
+            case 'green':
+                return {
+                    icon: people,
+                    color: '#69B4A0'
+                }
+            case 'purple':
+                return {
+                    icon: message,
+                    color: '#5A4DB2'
+                }
+            case 'red':
+                return {
+                    icon: puzzle,
+                    color: '#B6486D'
+                }
+            default:
+                return {
+                    icon: people,
+                    color: '#69B4A0'
+                }
+        }
+    }
+
+    const style = determineStyle(props.color)
+
+    return (
+        <Outer color={style.color}>
+            <div>
+                <img src={style.icon} alt="tip icon" />
+                <h4>{props.heading}</h4>
+            </div>
+            <p>{props.copy}</p>
+        </Outer>
+    )
+
+}
+
+
+export default TipCard
+
+const Outer = styled.div`
+    margin: 0 auto;
+	height: 14rem;
+	width: 33rem;
+	border-radius: 4px;
+    background-color: #fff;
+
+    > div:first-child {
+        padding: 12px 0px;
+        width: 85%;
+        margin: 0 auto;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        img {
+            margin-right: 7.5px;
+        }
+    }
+    
+    h4 {
+        color: ${(props: any) => props.color};
+        font-size: 18px;
+        font-weight: 500;
+    }
+
+    p {
+        width: 80%;
+        margin: 0 auto;
+        color: ${props => props.theme.offblack};
+    }
+`
--- src/containers/profile/index.tsx
+++ src/containers/profile/index.tsx
@@ -1,15 +1,82 @@
 import * as React from 'react';
+import styled from 'styled-components'
+import TipCard from '../../components/profile/TipCard'
 
+import pin from '../../img/icons/pin.svg'
+import editDot from '../../img/icons/editDot.svg'
+import cog from '../../img/icons/cog.svg'
 
 export default class Profile extends React.Component<{}, {}> {
 
     public render() {
         return (
             <div>
-                Profile
+                <Main>
+                    <img src="http://www.fillmurray.com/700/700" alt="your profile photo" />
+                    <h2>Bill, 67</h2>
+                    <Location>
+                        <img src={pin} alt="location" />
+                        Collingwood
+                    </Location>
+
+                    <Tabs>
+                        <div><img src={editDot} alt="edit profile" />Edit profile</div>
+                        <div><img src={cog} alt="account settings" />Settings</div>
+                    </Tabs>
+                </Main>
+                <TipCard color='red' heading='Meet more people' copy='People feel more comfortable meeting others that have 3 or more pictures on their profile.' >Yellow</TipCard>
             </div>
         )
     }
 
 
 }
+
+const Location = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    margin: 4px 0px 15px 0px;
+    color: ${props => props.theme.grey};
+    img {
+        height: 13px;
+        width: 9px;
+        margin-right: 6.5px;
+    }
+`
+
+const Tabs = styled.div`
+    width: 100%;
+    color: ${props => props.theme.blue};
+    display: flex;
+    justify-content: space-around;
+    > div {
+        padding: 15px 0px;
+        display: flex;
+        justify-content: space-around;
+        img {
+            margin-right: 12px;
+        }
+    }
+`
+
+const Main = styled.div`
+    padding-top: 45px;
+    display: grid;
+    justify-items: center;
+    background-color: #fff;
+    > img:first-child {
+        height: 15.8rem;
+        height: 15.8rem;
+        border-radius: 50%;
+        border: 4px solid ${props => props.theme.offwhite};
+        margin-bottom: 15px;
+    }
+
+    > h2 {
+        font-size: 2.8rem;
+        color: ${props => props.theme.purple};
+    }
+`
+
+
